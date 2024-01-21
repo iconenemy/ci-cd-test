8a9eb4a99a749820099d4019aec5b773a7e8c3d8
Ivan <iconenemy@gmail.com>
2024-01-21 14:51:53 +0200

    feat: Test

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,3 @@
-<<<<<<< HEAD
-=======
-import { useState } from 'react'
->>>>>>> d4dc5871c4ef72f38d4ecbc0b7404d203bec1c24
 import './App.css'
 
 function App() {
@@ -9,22 +5,7 @@ function App() {
 
   return (
     <>
-<<<<<<< HEAD
     <h1>tesxt</h1>
-=======
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
->>>>>>> d4dc5871c4ef72f38d4ecbc0b7404d203bec1c24
     </>
   )
 }
